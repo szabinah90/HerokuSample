4085ac44b49339c7a00378a4a2e7a46d27d0b1fc
Szabina Hazi <szabina.hazi@flowacademy.hu>
2018-05-28 17:35:03 +0200

    first commit

---
+++ demo-heroku/src/main/java/com/heroku/demoheroku/DemoHerokuApplication.java
@@ -0,0 +1,12 @@
+package com.heroku.demoheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHerokuApplication.class, args);
+	}
+}
